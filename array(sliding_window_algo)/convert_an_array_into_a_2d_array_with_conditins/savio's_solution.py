c17930129e18253a014fb238bb5cfc1f0c0d5edb
Savio09 <declanfortune5@gmail.com>
2025-06-10 23:43:35 -0700

    solved another problem

---
+++ array(sliding_window_algo)/convert_an_array_into_a_2d_array_with_conditins/savio's_solution.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        count = defaultdict(int)
+        res = []
+        for num in nums:
+            if count[num] >= len(res):
+                res.append([])
+            res[count[num]].append(num)
+            count[num] += 1
+        return res
