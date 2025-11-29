985e678f84f4075b5623bcd1eb7cfe637b6f7a2b
DoYunseo <ysdoh0209@khu.ac.kr>
2025-11-29 14:16:53 +0900

    today's easy POTD

---
+++ leetcode_daily(November_2025)/minimum_operations_to_make_array_sum_divisible_by_k/yunseo's_solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        s = sum(nums)
+        cnt = 0
+        
+        while s % k != 0:
+            s -= 1
+            cnt += 1
+        return cnt
